6318cec7969943a139c44e425bfa9038c39a1c4f
Amir Salehi <nik.amirsa@gmail.com>
2021-09-18 11:34:20 +0430

    control prevent enter alphabetical characters without class name

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-$(document).on("keydown", ".numericInput", function (e) {
-  // Allow: backspace, delete, tab, escape, enter and .
-
-  if (
-    $.inArray(e.keyCode, [46, 8, 9, 27, 13]) !== -1 ||
-    // Allow: Ctrl+A, Command+A
-    (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-    // Allow: home, end, left, right, down, up
-    (e.keyCode >= 35 && e.keyCode <= 40)
-  ) {
-    // let it happen, don't do anything
-    return;
-  }
-  // Ensure that it is a number and stop the keypress
-  if (
-    (e.shiftKey || e.keyCode < 48 || e.keyCode > 57) &&
-    (e.keyCode < 96 || e.keyCode > 105)
-  ) {
-    e.preventDefault();
-  }
-});
-
 $(document).ready(function () {
   const result = [];
 
@@ -55,6 +33,24 @@ $(document).ready(function () {
         $(e.target).prev().focus().val(value);
         $("#credit_card").val(result.join(""));
       }
+    }
+
+    if (
+      $.inArray(e.keyCode, [46, 8, 9, 27, 13]) !== -1 ||
+      // Allow: Ctrl+A, Command+A
+      (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+      // Allow: home, end, left, right, down, up
+      (e.keyCode >= 35 && e.keyCode <= 40)
+    ) {
+      // let it happen, don't do anything
+      return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if (
+      (e.shiftKey || e.keyCode < 48 || e.keyCode > 57) &&
+      (e.keyCode < 96 || e.keyCode > 105)
+    ) {
+      e.preventDefault();
     }
   });
 
