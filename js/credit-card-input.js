baea1c449341d81dfc26d39e0f8bfb646491a2fe
Amir Salehi <nik.amirsa@gmail.com>
2021-09-18 13:10:46 +0430

    improve js file

--- js/credit-card-input.js
+++ js/credit-card-input.js
@@ -34,17 +34,14 @@ $(".credit-card-inputs input").on("keydown", (e) => {
     }
   }
 
+  // prevent enter alphabetic keys
   if (
     $.inArray(e.keyCode, [46, 8, 9, 27, 13]) !== -1 ||
-    // Allow: Ctrl+A, Command+A
     (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-    // Allow: home, end, left, right, down, up
     (e.keyCode >= 35 && e.keyCode <= 40)
   ) {
-    // let it happen, don't do anything
     return;
   }
-  // Ensure that it is a number and stop the keypress
   if (
     (e.shiftKey || e.keyCode < 48 || e.keyCode > 57) &&
     (e.keyCode < 96 || e.keyCode > 105)
@@ -53,6 +50,7 @@ $(".credit-card-inputs input").on("keydown", (e) => {
   }
 });
 
+// reset button handler
 $(".credit-card-inputs__reset").on("click", function () {
   $(".credit-card-inputs input").val("");
   $("#credit_card_result").val("");
